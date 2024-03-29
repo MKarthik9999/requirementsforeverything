178b45379278c5ea59039086c6a7fab33e19986d
EC2 Default User <ec2-user@ip-172-31-33-234.eu-west-2.compute.internal>
2024-03-29 13:27:08 +0000

    Added distribution wise installation for jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -12,21 +12,41 @@ fi
 
 # Now proceed with installing Jenkins
 
-
-# Add Jenkins repository
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+install_jenkins_debian() {
+    echo "Installing Jenkins on Debian-based distribution..."
+    # Add Jenkins repository
+    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
 
-# Add Jenkins repository to sources list
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+    # Add Jenkins repository to sources list
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+      https://pkg.jenkins.io/debian binary/ | sudo tee \
+      /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt update
+    sudo apt install jenkins -y
+}
 
-# Update package index
-sudo apt update
+install_jenkins_redhat() {
+    echo "Installing Jenkins on Red Hat-based distribution..."
+    sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    sudo yum upgrade -y
+    sudo yum install jenkins -y
+}
 
-# Install Jenkins
-sudo apt install jenkins -y
+# Check for the presence of specific commands to determine the Linux distribution
+if command -v apt &> /dev/null; then
+    # Debian-based distributions (e.g., Ubuntu)
+    install_jenkins_debian
+elif command -v yum &> /dev/null; then
+    # Red Hat-based distributions (e.g., CentOS, Fedora)
+    install_jenkins_redhat
+else
+    # Unsupported distribution
+    echo "Unsupported distribution. Java installation aborted."
+    exit 1
+fi
 
 # Start Jenkins service
 sudo systemctl start jenkins
