bde103303f9455bb5c176c3237a026b82955b49d
karthikmms <showrykarthik@gmail.com>
2024-04-01 16:44:51 +0000

    Changed the Tomcat version from 10 to 9

--- tomcat.sh
+++ tomcat.sh
@@ -16,31 +16,27 @@ install_Tomcat_debian() {
     echo "Installing Tomcat on Debian-based distribution..."
     
     sudo apt update -y
-    sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.19/bin/apache-tomcat-10.1.19.tar.gz
-    sudo tar xvzf apache-tomcat-10.1.19.tar.gz
-    sudo rm -rf apache-tomcat-10.1.19.tar.gz
-    sudo mv apache-tomcat-10.1.19/ /opt/tomcat
+    sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.87/bin/apache-tomcat-9.0.87.tar.gz
+    sudo tar xvzf apache-tomcat-9.0.87.tar.gz
+    sudo rm -rf apache-tomcat-9.0.87.tar.gz
+    sudo mv apache-tomcat-9.0.87/ /opt/tomcat
     # creating linked files for tomcat startup.sh and shutdown.sh so that we can access them anywhere from the system rather
     # than coming and accessing only from the folder
     sudo ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/starttomcat
     sudo ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/stoptomcat
-    sudo cd ~
 }
 
 install_Tomcat_redhat() {
     echo "Installing Tomcat on Red Hat-based distribution..."
     sudo yum update -y
-    sudo cd /opt/
-    sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.19/bin/apache-tomcat-10.1.19.tar.gz
-    sudo tar xvzf apache-tomcat-10.1.19.tar.gz
-    sudo rm -rf apache-tomcat-10.1.19.tar.gz
-    sudo mv apache-tomcat-10.1.19/ /opt/tomcat
-    # creating linked files for tomcat startup.sh and shutdown.sh so that we can access them anywhere from the system rather 
+    sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.87/bin/apache-tomcat-9.0.87.tar.gz
+    sudo tar xvzf apache-tomcat-9.0.87.tar.gz
+    sudo rm -rf apache-tomcat-9.0.87.tar.gz
+    sudo mv apache-tomcat-9.0.87/ /opt/tomcat
+    # creating linked files for tomcat startup.sh and shutdown.sh so that we can access them anywhere from the system rather
     # than coming and accessing only from the folder
     sudo ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/starttomcat
     sudo ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/stoptomcat
-    sudo cd ~
-
 }
 
 # Check for the presence of specific commands to determine the Linux distribution
